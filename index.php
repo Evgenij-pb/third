135bafa35bb88dc8cd87db5a7cad025c5e45176d
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-09-30 16:56:34 +0300

    add autoload

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php
+
+spl_autoload_register(function($className){
+    $classFile = 'vendor'. DIRECTORY_SEPARATOR . str_replace("\\", DIRECTORY_SEPARATOR, $className) . '.php';
+    if(file_exists($classFile)){
+        include_once $classFile;
+        return true;
+    }
+    return false;
+});
